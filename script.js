310584f22098843e6f1d6010aa6828a36363c647
lucioacri <lucioacri@gmail.com>
2025-04-15 13:57:23 +0200

    add cards

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
+const membersCards = document.getElementById("members-cards");
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "img/female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "img/male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "img/female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "img/male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
+    img: "img/female3.png",
+  },
 ];
+let cardsHTML = ``;
+for (const member of teamMembers) {
+  const { name, role, email, img } = member;
+  console.log(member);
+  cardsHTML += `
+    <div class=col>
+      <div class=card-image>
+       <img src="./${img}" alt="${name}">
+      </div>
+      <div class=card-text>
+       <h3> ${name} </h3>
+       <p>${role}</p>
+       <p>${email}</p>
+      </div>
+    </div>`;
+}
+membersCards.innerHTML = cardsHTML;
